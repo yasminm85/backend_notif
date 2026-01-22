d9875cfd9ba349d5d42e44c2e3edcd130b5f3e09
Yasmin <yasminmutiarabintang@gmail.com>
2026-01-22 09:28:15 +0700

    adding cors

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['http://localhost:3001'],
+  origin: ['http://localhost:3001', 'https://frontend-navnotif.vercel.app/'],
   credentials: true
 }));
 
