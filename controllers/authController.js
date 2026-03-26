907c7dca8b570e7f5856f8ee04c0dd900d27d349
Yasmin <yasminmutiarabintang@gmail.com>
2026-03-26 16:04:13 +0700

    transporter undefined

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {transporter} = require('../config/nodemailer');
 
 // register
 const register = async (req, res) => {
